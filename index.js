68138e3f08744e53f499195f1b6498c1fe256018
khushboo <khushboo.rathore@grevity.in>
2021-01-09 17:43:35 +0530

    removed node m

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let port =process.env.port || 8000;
 
 app.get('/', function (req, res) {
     res.send("Hello World");
-    res.end();
+   
 })
 
 app.get('/players', function (req, res) {
     res.send(importPlayers);
-    res.end();
+  
 })
 
 app.listen(port, () => {
+    console.log(port)
     console.log('Project Host on Heroku on port 8000')
 });
