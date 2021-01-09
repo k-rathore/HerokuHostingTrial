c1c079593f2eb7d2a33438186e8ad87efb30fb38
khushboo <khushboo.rathore@grevity.in>
2021-01-09 13:09:03 +0530

    Latest Comment

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express=require('express');
-const app=express();
-const importPlayers=require('./players.json');
-let port=8000;
+const express = require('express');
+const app = express();
+const importPlayers = require('./players.json');
+let port =process.env.port || 8000;
 
-app.get("/",(req,res)=>{
+app.get('/', function (req, res) {
     res.send("Hello World");
     res.end();
 })
 
-app.get("/players",(req,res)=>{
-res.send(importPlayers);
-res.end();
+app.get('/players', function (req, res) {
+    res.send(importPlayers);
+    res.end();
 })
-app.listen(port,()=>{
-    console.log('Project Host on Heroku on port 8000');
+
+app.listen(port, () => {
+    console.log('Project Host on Heroku on port 8000')
 });
